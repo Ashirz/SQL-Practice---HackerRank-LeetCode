8486e4bf239580a87e56f6dd5c0b190b098880ca
Asiya <121093841+Ashirz@users.noreply.github.com>
2026-03-01 11:31:00 +0000

    Create revising-the-select-query-2.sql
    Platform: HackerRank
    URL: https://www.hackerrank.com
    Difficulty: Easy
    Category: Basic Select

---
+++ HackerRank/Basic-Select/revising-the-select-query-2.sql
@@ -0,0 +1,10 @@
+/*
+Title: Revising the Select Query II
+Platform: HackerRank
+Category: Basic Select
+URL: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+Dialect: MySQL / DB2
+*/
+
+Select name from city 
+where population >120000 and countrycode = 'USA';
